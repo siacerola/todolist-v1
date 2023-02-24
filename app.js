57c0aa317e402bf976030742e2a44483a629a5e7
siacerola <luq5nvd@gmail.com>
2023-02-24 16:35:40 +0700

    add work server

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 
-let items =[];
-
+let items =["Buy Food","Cook Food","Eat Food"];
+let workItems =[];
 
 app.get("/",function(req,res){
   
@@ -22,16 +23,32 @@ app.get("/",function(req,res){
     let day = today.toLocaleDateString("en-US", option);
 
     res.render("list",{
-        kindOfDay:day,
+        listTitle:day,
         newListItems:items
     });
 });
 
 app.post("/",(req,res)=>{
+    // console.log(req.body)
 
-    items.push(req.body.newItem);
-    res.redirect("/");
+    let item = req.body.newItem;
+    if(req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect("/work")
+    } else {
+        items.push(req.body.newItem);
+        res.redirect("/");
+    }
+});
 
+app.get("/work",function(req,res){
+    res.render("list", {listTitle: "Work List",newListItems:workItems});
+});
+
+app.post("/work",function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 });
 
 app.listen(3000, function(){
